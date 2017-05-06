9ffe5fa2c4ec21d07b96c28c2f79df103162a40c
Joshua Rochotte <joshrochotte@intelligentinteractions.org>
2017-05-05 22:09:19 -0400

    facebook

--- Facebook.R
+++ Facebook.R
@@ -2,16 +2,18 @@
 library(devtools)
 #install_github("Rfacebook", "pablobarbera", subdir="Rfacebook")
 require("Rfacebook")
+#671755522900353
+#1803067243268561
 fb_oauth <- fbOAuth(app_id="1803067243268561", app_secret="2c0945e50ee5a4936c87cc3f09b4918f",extended_permissions = TRUE)
 save(fb_oauth, file="fb_oauth")
 load("fb_oauth")
 me <- getUsers("me",token=fb_oauth)
 my_likes <- getLikes(user="me", token=fb_oauth)
+dude <- getFriends(token=fb_oauth, simplify = FALSE)
 #updateStatus("This is me Posting from my Analytics App",token=fb_oauth)
-searchPagesGay <- searchPages("", token=fb_oauth, n=200)
-View(searchPagesGay)
-searchDapl <-searchGroup(string="cubs",token=fb_oauth, n = 1000)
-#getPage(pageid , token, n = n)
-pages <- searchPages( string="cubs", token=fb_oauth, n=1000)
-map('usa') # create a map 
-with(filter(pages), points(longitude, latitude))
+# View(searchPagesGay)
+# searchDapl <-searchGroup(string="cubs",token=fb_oauth, n = 1000)
+# #getPage(pageid , token, n = n)
+# pages <- searchPages( string="cubs", token=fb_oauth, n=1000)
+# map('usa') # create a map 
+# with(filter(pages), points(longitude, latitude))
